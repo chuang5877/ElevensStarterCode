79c2f6d73fa9cc738aff92d136414138ea1a6e17
chuang5877 <chuang5877@bths.edu>
2019-02-13 08:35:21 -0500

    yes

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -34,7 +34,9 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		this.rank=cardRank;
+		this.suit=cardSuit;
+		this.pointValue=cardPointValue;
     }
 
 
